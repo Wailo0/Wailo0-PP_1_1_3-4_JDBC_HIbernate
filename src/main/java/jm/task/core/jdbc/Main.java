653e20c4f576ce132133beeecaef7d70dd19ebd5
Wailo0 <iymashev@gmail.com>
2023-01-07 15:30:20 +0300

    commi1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
-public class Main {
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main extends Util {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl() ;
+
+//        us.createUsersTable();
+//        us.dropUsersTable();
+//        us.saveUser("Grisha", "Udin", (byte)32);
+//        us.saveUser("Fedor", "Aravadin", (byte)21);
+//        us.saveUser("Liner", "Pau", (byte)8);
+//        us.removeUserById(1);
+
+//        for (User e : us.getAllUsers()) {
+//            System.out.println(e);
+//        }
+
+//        us.cleanUsersTable();
+
+
     }
 }
