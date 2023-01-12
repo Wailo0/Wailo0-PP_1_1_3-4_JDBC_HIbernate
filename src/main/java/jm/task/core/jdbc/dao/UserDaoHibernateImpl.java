f9770bcfc1c9982c64517ce82c4ab6bca2997966
Wailo0 <iymashev@gmail.com>
2023-01-12 07:07:34 +0300

    c4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,29 +4,41 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl extends Util implements UserDao {
     SessionFactory sessionFactory = getSessionFactory();
 
-    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
-
-
-    public UserDaoHibernateImpl() {
-
-    }
-
+    public UserDaoHibernateImpl() {}
 
     @Override
     public void createUsersTable() {
-        daoJDBC.createUsersTable();
+        String createTable = "CREATE TABLE IF NOT EXISTS " + Util.DB_NAME + ".usertable (\n" +
+                " `id` BIGINT NOT NULL AUTO_INCREMENT,\n" +
+                " `name` VARCHAR(45) NULL,\n" +
+                " `lastName` VARCHAR(45) NULL,\n" +
+                " `age` INT(3) NULL,\n" +
+                "PRIMARY KEY(`id`))\n" +
+                "ENGINE = InnoDB\n" +
+                "DEFAULT CHARACTER SET = utf8;\n";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery(createTable).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("Table has been create");
     }
 
     @Override
     public void dropUsersTable() {
-        daoJDBC.dropUsersTable();
+        String dropTable = "DROP TABLE IF EXISTS " + Util.DB_NAME + ".usertable";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.createSQLQuery(dropTable).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("Table has been dropped");
     }
 
     @Override
